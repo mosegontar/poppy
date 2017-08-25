819ad3a65a4f4acaf9a9f91718f305a5fb55688a
Alex Gontar <mosegontar@gmail.com>
2017-08-25 00:04:11 -0400

    Set editor with arg

--- poppy.py
+++ poppy.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import subprocess
 
+
 EXTENSION_MAP = {
     'py': 'python',
     'rb': 'ruby'
@@ -9,23 +10,29 @@ EXTENSION_MAP = {
 
 class Poppy(object):
 
-    def __init__(self, fname='_poppy.py'):
-        self.fname = fname
-        self.editor = os.environ.get('POPPY_EDITOR', 'vim')
+    def __init__(self, editor):
+        self.fname = '_poppy.py'
+        self.editor = editor
 
         # create the file if it doesn't exist
         # and close file immediately after
         open(self.fname, 'a').close()
 
-    def edit(self):
+    def edit(self, new=False, run=False):
+
+        if new:
+            self._new()
 
         cmd = [self.editor, self.fname]
         subprocess.call(cmd)
 
-    def new(self):
+        if run:
+            self._run()
+
+    def _new(self):
         open(self.fname, 'w').close()
 
-    def run(self):
+    def _run(self):
 
         extension = self.fname.split('.')[1]
         if extension not in EXTENSION_MAP.keys():
@@ -37,20 +44,22 @@ class Poppy(object):
 
 
 def main():
-    poppy = Poppy()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--new', help="Create new blank poppy file", action='store_true')
-    parser.add_argument('-r', '--run', help="Run interpreter for file after editing", action='store_true')
+
+    parser.add_argument('-  e', '--editor', default='vim')
+    parser.add_argument('-n', '--new',
+                        help="Create new blank poppy file",
+                        action='store_true')
+    parser.add_argument('-r', '--run',
+                        help="Run interpreter for file after editing",
+                        action='store_true')
+
     args = parser.parse_args()
 
-    if args.new:
-        poppy.new()
+    poppy = Poppy(args.editor)
+    poppy.edit(args.new, args.run)
 
-    poppy.edit()
-
-    if args.run:
-        poppy.run()
 
 if __name__ == '__main__':
     main()
